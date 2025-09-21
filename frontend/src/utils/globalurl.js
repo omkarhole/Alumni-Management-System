9a35c91f3fc8307b784f89b8158d4cd60f7f1f01
VedantBudhabaware <vedantbudhabaware@gmail.com>
2025-09-21 20:06:02 +0530

    Added all basic frontend

---
+++ frontend/src/utils/globalurl.js
@@ -0,0 +1,5 @@
+export const baseUrl="http://localhost:5000/api/admin";
+export const authUrl="http://localhost:5000/auth";
+
+// export const baseUrl="https://alumni-server-alpha.vercel.app/api/admin";
+// export const authUrl="https://alumni-server-alpha.vercel.app/auth";
