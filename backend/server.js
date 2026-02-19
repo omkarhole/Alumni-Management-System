1e5e09a2e6c121ac663361cea1d4a71fe0555f33
omkar hole <omkarhole314@gmail.com>
2026-02-19 11:58:33 +0530

    fix packages

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.use('/api/contact', contactRouter);
 
 /* =========================
    TEST ROUTE
-
+ ========================= */
 app.post('/direct-test', (req, res) => {
     res.send("DIRECT POST WORKING");
 });
@@ -115,7 +115,7 @@ app.use(errorHandler);
 
 /* =========================
    START SERVER
-
+ ========================= */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
